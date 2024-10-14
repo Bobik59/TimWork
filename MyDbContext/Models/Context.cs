f188065a413913475d007886e45004d5a3aa8372
top_acdemy_student <top_acdemy_student@07-00>
2024-10-14 18:15:04 +0700

    Добавьте файлы проекта.

---
+++ MyDbContext/Models/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp2.Models;
+
+internal class Context : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Group> Groups { get; set; }
+
+    public Context() 
+    {
+        Database.Migrate();
+        //Database.EnsureDeleted();
+        //Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connString = @"Server=(localdb)\MSSQLLocalDB;
+                                    Database=Popa;
+                                    Trusted_Connection=True;";
+        optionsBuilder.UseSqlServer(connString);
+    }
+}
---
+++ MyDbContext/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDbContext.Models
+{
+    public class Game
+    {
+        int Id;
+        public string GameName {get; set; } = String.Empty;
+        public string Firm { set; get; }
+        public string? StyleGame { get; set; }
+        public DateTime data { set; get; }
+    }
+}
